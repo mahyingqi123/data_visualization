cd1e76f343983157927d05044c01abe65d6ac68b
mahyingqi123 <110000685+mahyingqi123@users.noreply.github.com>
2023-10-11 09:05:06 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -7,3 +7,18 @@ var vg_2 = "radial_chart.vg.json";
 vegaEmbed("#radial_chart", vg_2, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
+
+var vg_3 = "stacked_bar.vg.json";
+vegaEmbed("#stacked_bar", vg_3, {"actions": false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_4, {"actions": false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_5 = "heatmap.vg.json";
+vegaEmbed("#heatmap", vg_5, {"actions": false}).then(function(result) {
+
+}).catch(console.error);
