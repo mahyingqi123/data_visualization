cd41196b7011a27f60a07e3bd1a921587a5511ba
mahyingqi123 <110000685+mahyingqi123@users.noreply.github.com>
2023-10-17 23:12:30 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,22 +3,23 @@ vegaEmbed("#chloro_map", vg_1, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
-var vg_2 = "radial_chart.vg.json";
-vegaEmbed("#radial_chart", vg_2, {"actions": false}).then(function(result) {
-
-}).catch(console.error);
 
 var vg_3 = "stacked_bar.vg.json";
 vegaEmbed("#stacked_bar", vg_3, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
-var vg_4 = "bubble_plot.vg.json";
+var vg_4 = "line_rank.vg.json";
 vegaEmbed("#bubble_plot", vg_4, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var vg_5 = "heatmap.vg.json";
 vegaEmbed("#heatmap", vg_5, {"actions": false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_6 = "multiline.vg.json";
+vegaEmbed("#plot", vg_6, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
